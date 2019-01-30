0ef578f1e0d7bdbd3ed5cf499d3f18d6034cd51d
Sully <andrew.sullivan@procore.com>
2019-01-30 09:22:07 -0800

    Add users route and register button

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/register'
+  resources 'users', only: :index
+  post 'users/register'
 end
--- spec/features/user_registers_spec.rb
+++ spec/features/user_registers_spec.rb
@@ -2,9 +2,12 @@ require "rails_helper"
 
 RSpec.describe "User registers" do
   scenario "visiting the registration page" do
-    visit "/users/register"
+    visit "/users"
 
     expect(page).to have_field("email")
+    fill_in(:email, with: "email@email.com")
+    click_button("Register")
+    expect(ActionMailer::Base.deliveries.size).to eq(1)
   end
 end
 
