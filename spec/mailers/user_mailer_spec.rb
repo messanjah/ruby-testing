9aa5bea5bbe523f8fb76fc5d836f7a307ad7e69c
Brendan Jones <brendan.jones@procore.com>
2019-01-31 09:04:06 -0800

    add another descrieb to register

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
+RSpec::Matchers.define :have_subject do |expected|
+  match do |actual|
+    mail_message = actual
+    mail_message.subject == expected
+  end
+end
+
 RSpec.describe UserMailer do
   describe '#register' do
     it 'generates an email' do
@@ -8,5 +15,12 @@ RSpec.describe UserMailer do
     it 'delivers to correct recipient' do
       expect(UserMailer.with(email: 'procore@procore.com').register.to).to contain_exactly 'procore@procore.com'
     end
+  end
+
+  describe '#register' do
+    subject { described_class.with(email: email).register }
+    let(:email) { 'procore@procore.com' }
+    it { is_expected.to be }
+    it { is_expected.to have_subject 'Hello world!' }
   end
 end
